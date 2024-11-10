70a3d2cc50d0ade47e1d44ad93c5aaafa6e9c71d
Duc Le <leducst1@gmail.com>
2024-11-10 09:11:28 +0000

    m

--- provider.tf
+++ provider.tf
@@ -9,4 +9,8 @@ terraform {
 
 provider "azurerm" {
   features{}
+  client_id     = "<APPLICATION_ID>"   # Replace with your application's ID
+  client_secret = "<CLIENT_SECRET>"     # Replace with your client secret
+  tenant_id     = "<TENANT_ID>"         # Replace with your tenant ID
+  subscription_id = "<SUBSCRIPTION_ID>" # Replace with your subscription ID
 }
