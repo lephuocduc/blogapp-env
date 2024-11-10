1567b4dbc78baa4e96ff9a0fa3d2f58d1f988574
Duc Le <leducst1@gmail.com>
2024-11-10 14:57:16 +0000

    m

--- provider.tf
+++ provider.tf
@@ -15,4 +15,9 @@ provider "azurerm" {
   }
 
   ### Service Principle values need to be exported
+    #export ARM_CLIENT_ID="5e71b546-1fbb-4723-af47-545d1e190e96"
+    #export ARM_CLIENT_SECRET="secret"
+    #export ARM_TENANT_ID="a672f74e-032d-49f3-b705-1b37bf6e9bfc"
+    #export ARM_SUBSCRIPTION_ID="17e4c67a-a30c-40f3-9663-6071a6410826"
+
 }
