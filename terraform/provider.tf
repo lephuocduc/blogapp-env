f79212f3fbba36f66e2e1a3d0cca6958b45d1640
Duc Le <leducst1@gmail.com>
2024-11-11 02:53:54 +0000

    m

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,10 +8,7 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
+  features {}
   }
 
   ### Service Principle values need to be exported
@@ -19,5 +16,3 @@ provider "azurerm" {
     #export ARM_CLIENT_SECRET="secret"
     #export ARM_TENANT_ID="a672f74e-032d-49f3-b705-1b37bf6e9bfc"
     #export ARM_SUBSCRIPTION_ID="17e4c67a-a30c-40f3-9663-6071a6410826"
-
-}
---
+++ terraform/variable.tf
@@ -0,0 +1,11 @@
+variable cluster_name {
+  type        = string
+  default     = "blogapp-cluster"
+  description = "description"
+}
+
+variable branch {
+  type        = string
+  default     = "dev"
+  description = "description"
+}
