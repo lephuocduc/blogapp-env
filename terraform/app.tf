744df2b64060c230fbcf64406e71579c31d84db3
Duc Le <leducst1@gmail.com>
2024-11-11 09:48:21 +0000

    m

--- terraform/app.tf
+++ terraform/app.tf
@@ -20,26 +20,3 @@ resource "kubernetes_pod" "test" {
     }
   }
 }
-
-resource "kubernetes_pod" "test2" {
-  metadata {
-    name = "terraform-example2"
-  }
-
-  spec {
-    container {
-      image = "nginx:1.21.6"
-      name  = "example"
-
-      env {
-        name  = "environment"
-        value = "test"
-      }
-
-      port {
-        container_port = 80
-      }
-
-    }
-  }
-}
