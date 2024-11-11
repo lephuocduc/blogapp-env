1c8947abeeadf901e218a2a906de68acd2c9e63e
Duc Le <leducst1@gmail.com>
2024-11-11 18:00:03 +0000

    m

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D2plds_v6"
+    vm_size    = "Standard_D2as_v4"
 
     upgrade_settings {
       drain_timeout_in_minutes      = 0
