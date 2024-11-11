fe14125d4a45ae34826fcc7bd84c2558afd823e7
Duc Le <leducst1@gmail.com>
2024-11-11 03:39:15 +0000

    m

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,6 @@
 terraform {
   backend "azurerm" {
     #terraform backend will be stored on Azure Storage Container
-    resource_group_name   = "Duc-RG1"
-    storage_account_name   = "ducleterraformbackend"
-    container_name         = "terraform-backend"
-    key                    = "terraform.tfstate"
+
   }
 }
