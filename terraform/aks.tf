60264f1ab794c36589d0dd74da33d177d6816bd8
Duc Le <leducst1@gmail.com>
2024-11-11 09:31:26 +0000

    m

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -15,11 +15,21 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   tags = {
-    Environment = "Production"
+    Environment = "${var.branch}"
   }
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
-}
+  output "client_certificate" {
+    value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+    sensitive = true
+  }
+
+  output "client_key" {
+    value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+    sensitive = true
+  }
+
+  output "cluster_ca_certificate" {
+    value     = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+    sensitive = true
+  }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,3 +16,10 @@ provider "azurerm" {
     #export ARM_CLIENT_SECRET="secret"
     #export ARM_TENANT_ID="a672f74e-032d-49f3-b705-1b37bf6e9bfc"
     #export ARM_SUBSCRIPTION_ID="17e4c67a-a30c-40f3-9663-6071a6410826"
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
