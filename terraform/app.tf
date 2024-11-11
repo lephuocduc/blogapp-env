7dd1f32b787f5919fe1285da7b87ccfaba4c7aa6
Duc Le <leducst1@gmail.com>
2024-11-11 10:29:42 +0000

    m

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,45 +1,11 @@
-resource "kubernetes_pod" "test" {
-  metadata {
-    name = "terraform-example"
-  }
-
-  spec {
-    container {
-      image = "nginx:1.21.6"
-      name  = "example"
-
-      env {
-        name  = "environment"
-        value = "test"
-      }
-
-      port {
-        container_port = 80
-      }
-
-    }
-  }
-}
-
-resource "kubernetes_pod" "test2" {
-  metadata {
-    name = "terraform-example2"
-  }
-
-  spec {
-    container {
-      image = "nginx:1.21.6"
-      name  = "example"
-
-      env {
-        name  = "environment"
-        value = "test"
-      }
-
-      port {
-        container_port = 80
-      }
-
-    }
-  }
-}
+ data "kubectl_file_documents" "apps" {
+    content = file("../manifests/argocd/apps.yaml")
+ }
+ resource "kubectl_manifest" "apps" {
+  depends_on = [
+    kubectl_manifest.argocd,
+  ]
+  for_each  = data.kubectl_file_documents.apps.manifests
+  yaml_body = each.value
+  override_namespace = "argocd"
+ }
--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,4 +1,21 @@
-# resource "time_sleep" "wait_30_seconds" {
-# depends_on = [resource.azurerm_kubernetes_cluster.aks]
-# create_duration = "30s"
-# }
+data "kubectl_file_documents" "namespace" {
+ content = file("../manifests/argocd/namespace.yaml")
+ }
+ data "kubectl_file_documents" "argocd" {
+ content = file("../manifests/argocd/install.yaml")
+ }
+
+ resource "kubectl_manifest" "namespace" {
+  for_each  = data.kubectl_file_documents.namespace.manifests
+  yaml_body = each.value
+  override_namespace = "argocd"
+ }
+
+ resource "kubectl_manifest" "argocd" {
+  depends_on = [
+    kubectl_manifest.namespace,
+  ]
+  for_each  = data.kubectl_file_documents.argocd.manifests
+  yaml_body = each.value
+  override_namespace = "argocd"
+ }
