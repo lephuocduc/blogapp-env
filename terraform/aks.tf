7d32b774f219b77289e300be2dca4f2e3ee56dc8
Duc Le <leducst1@gmail.com>
2024-11-11 09:44:24 +0000

    m

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -6,8 +6,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 3
+    node_count = 1
     vm_size    = "Standard_D2plds_v6"
+
+    upgrade_settings {
+      drain_timeout_in_minutes      = 0
+      max_surge                     = "10%"
+      node_soak_duration_in_minutes = 0 
+    }
   }
 
   identity {
@@ -18,18 +24,3 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Environment = "${var.branch}"
   }
 }
-
-  output "client_certificate" {
-    value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
-    sensitive = true
-  }
-
-  output "client_key" {
-    value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
-    sensitive = true
-  }
-
-  output "cluster_ca_certificate" {
-    value     = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
-    sensitive = true
-  }
